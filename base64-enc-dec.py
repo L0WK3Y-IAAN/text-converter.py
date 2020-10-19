0ea1b15f554e777de2cdb3bd91f967b6da2cd4a1
Ichor Official <l0wk3y@iaansec.net>
2020-10-19 12:58:50 -0400

    multi platform version (base64)

--- base64-enc-dec.py
+++ base64-enc-dec.py
@@ -1,68 +1,124 @@
 import base64
 import subprocess
+from sys import platform
 
-while True:
-    try: 
-        subprocess.call(['cls'],shell=True) #Change 'cls' to 'clear' for linux
-        modeSelect = input('Select a conversion mode: \n1: Encode\n2: Decode\n0: Exit\n\nSelection: ')
-        if modeSelect == '1':
+def osCheck():
+    if platform == "win32":
 
-            def enc():
-                subprocess.call(['cls'], shell=True)
-                userInput = input("Enter plain text here: ")
-                subprocess.call(['cls'], shell=True)
+        while True:
+            try:
+                subprocess.call(['cls'],shell=True) 
+                modeSelect = input('Select a conversion mode: \n1: Encode\n2: Decode\n0: Exit\n\nSelection: ')
+                if modeSelect == '1':
 
-                """
-                Uses base64 library to encode and decode text
-                https://docs.python.org/3/library/base64.html
-                """
-                b64enc = base64.b64encode(userInput.encode('utf-8'))
+                    def enc():
+                        subprocess.call(['cls'], shell=True)
+                        userInput = input("Enter plain text here: ")
+                        subprocess.call(['cls'], shell=True)
 
-                print('Original Text: ' + userInput + '\n\nEncoded Conversions\n_______________________\n\nBase64 Encoded:', b64enc.decode())
+                        """
+                        Uses base64 library to encode and decode text
+                        https://docs.python.org/3/library/base64.html
+                        """
+                        b64enc = base64.b64encode(userInput.encode('utf-8'))
 
-                input('\n\n\nPress Enter to continue...')
+                        print('Original Text: ' + userInput + '\n\nEncoded Conversions\n_______________________\n\nBase64 Encoded:', b64enc.decode())
 
-            enc()
-            
+                        input('\n\n\nPress Enter to continue...')
 
-        if modeSelect == '2':
-            subprocess.call(['cls'],shell=True)
-            def b64dec():
-                try:
+                    enc()
+
+
+                if modeSelect == '2':
+                    subprocess.call(['cls'],shell=True)
+                    def b64dec():
+                        try:
+                            subprocess.call(['cls'], shell=True)
+                            userInput = input("Enter encoded text here: ")
+                            subprocess.call(['cls'], shell=True)
+
+                            """
+                            Uses base64 library to encode and decode text
+                            https://docs.python.org/3/library/base64.html
+                            """
+                            b64dec = base64.b64decode(userInput)
+
+                            print('Converted Text: ' + b64dec.decode() + '\nBase64 Encoded: ', userInput)
+                            input('\n\n\nPress Enter to continue...')
+
+
+                        except:
+                            input('Incorrect text format, please enter encoded Base64.\n\nPress enter')
+                    b64dec()  
+
+
+                if modeSelect == '0':
                     subprocess.call(['cls'], shell=True)
-                    userInput = input("Enter encoded text here: ")
-                    subprocess.call(['cls'], shell=True)
+                    break
 
-                    """
-                    Uses base64 library to encode and decode text
-                    https://docs.python.org/3/library/base64.html
-                    """
-                    b64dec = base64.b64decode(userInput)
+            except KeyboardInterrupt:
+                subprocess.call(['cls'], shell=True)
+                input('Program Terminated. Press Enter to continue...')
+                subprocess.call(['cls'], shell=True)
 
-                    print('Converted Text: ' + b64dec.decode() + '\nBase64 Encoded: ', userInput)
+                break
+    if platform == "linux" or platform == "linux2":
 
-                except:
-                    input('Incorrect text format, please enter encoded Base64.\n\nPress enter')
+        while True:
+            try:
+                subprocess.call(['clear'],shell=True) 
+                modeSelect = input('Select a conversion mode: \n1: Encode\n2: Decode\n0: Exit\n\nSelection: ')
+                if modeSelect == '1':
 
-            b64dec()
+                    def enc():
+                        subprocess.call(['clear'], shell=True)
+                        userInput = input("Enter plain text here: ")
+                        subprocess.call(['clear'], shell=True)
 
-        if modeSelect == '0':
-            subprocess.call(['cls'], shell=True)
-            break
-        
-    except KeyboardInterrupt:
-        subprocess.call(['cls'], shell=True)
-        input('Program Terminated. Press Enter to continue...')
-        subprocess.call(['cls'], shell=True)
+                        """
+                        Uses base64 library to encode and decode text
+                        https://docs.python.org/3/library/base64.html
+                        """
+                        b64enc = base64.b64encode(userInput.encode('utf-8'))
 
-        break
+                        print('Original Text: ' + userInput + '\n\nEncoded Conversions\n_______________________\n\nBase64 Encoded:', b64enc.decode())
 
+                        input('\n\n\nPress Enter to continue...')
 
-
-
-    
-
+                    enc()
 
 
+                if modeSelect == '2':
+                    subprocess.call(['clear'],shell=True)
+                    def b64dec():
+                        try:
+                            subprocess.call(['clear'], shell=True)
+                            userInput = input("Enter encoded text here: ")
+                            subprocess.call(['clear'], shell=True)
 
-    
+                            """
+                            Uses base64 library to encode and decode text
+                            https://docs.python.org/3/library/base64.html
+                            """
+                            b64dec = base64.b64decode(userInput)
+
+                            print('Converted Text: ' + b64dec.decode() + '\nBase64 Encoded: ', userInput)
+                            input('\n\n\nPress Enter to continue...')
+
+
+                        except:
+                            input('Incorrect text format, please enter encoded Base64.\n\nPress enter')
+                    b64dec()  
+
+
+                if modeSelect == '0':
+                    subprocess.call(['clear'], shell=True)
+                    break
+
+            except KeyboardInterrupt:
+                subprocess.call(['clear'], shell=True)
+                input('Program Terminated. Press Enter to continue...')
+                subprocess.call(['clear'], shell=True)
+
+                break
+osCheck()
